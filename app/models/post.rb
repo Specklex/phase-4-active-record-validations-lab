c5f96f51e527c1de20ce1e409150a1ea6386f25d
Jaden Lund <lundjaden@gmail.com>
2022-09-25 15:02:28 -0500

    commit tree 2544607104073464f5cffa183866a758e7c02dfb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w[Fiction Non-Fiction] }
+  validate :title_words
+
+  VALID_WORDS = ["Won't Believe", "Secret", "Top", "Guess"]
+
+  def title_words
+    if title != nil
+      if VALID_WORDS.any? { |w| title.include?(w) }
+        nil
+      else
+        errors.add(:title, "Please try again")
+      end
+    end
+  end
 end
